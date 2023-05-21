0963d6f1c3136f79bb1969956d768e9f05b4bd97
jeffmikhail <mikhailcruz78@gmail.com>
2023-05-21 21:25:44 +0800

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            string response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max);
+
+                while (guess != number)
+                {
+                    Console.WriteLine($"Guess a number between {min}-{max}.");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Guess: {guess}");
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine($"{guess} is too high");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine($"{guess} is too low");
+                    }
+
+                    guesses++;
+                }
+
+                Console.WriteLine($"Number: {number}");
+                Console.WriteLine("YOU WIN!!!");
+                Console.WriteLine($"Guesses: {guesses}");
+
+                Console.Write("Would you like to play again? (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing!!!");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
